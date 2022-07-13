1cf9f44b18529fa296cd6c9184721543091303b9
immprada <im.mprada@gmail.com>
2022-07-13 14:43:41 -0500

    Add organisms: UserLogForm

---
+++ src/components/organisms/UserLogForm/UserLogForm.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import {
+  RegisterForm,
+} from '../../molecules';
+import './styles.scss';
+
+const UserLogForm = ({}) => {
+
+  return (
+    <div className="user-log-form">
+      <RegisterForm />
+    </div>
+  )
+};
+
+export default UserLogForm;
