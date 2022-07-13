47a36f2e92d62e92df277c718bca7b4c8e08942f
immprada <im.mprada@gmail.com>
2022-07-13 14:44:09 -0500

    Add icons: Movie

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RegisterForm} from './components/molecules';
+import {UserLogForm} from './components/organisms'
 import './assets/styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -8,5 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RegisterForm />
+    <UserLogForm />
   </React.StrictMode>
 );
