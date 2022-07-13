94d559d5c22fd0fc709f73702260e40bec244537
immprada <im.mprada@gmail.com>
2022-07-13 14:53:07 -0500

    Add organisms: UserLogForm

--- src/components/organisms/UserLogForm/UserLogForm.jsx
+++ src/components/organisms/UserLogForm/UserLogForm.jsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect } from 'react';
 import {
   RegisterForm,
 } from '../../molecules';
+import {
+  Movie,
+} from '../../../assets/icons'
 import './styles.scss';
 
 const UserLogForm = ({}) => {
 
   return (
     <div className="user-log-form">
+      <div className="user-log-form__logo">
+        <Movie />
+      </div>
       <RegisterForm />
     </div>
   )
