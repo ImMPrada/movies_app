741541e0fc28f56f4fddad4b2dc5a7e31ac980f6
immprada <im.mprada@gmail.com>
2022-07-12 22:45:31 -0500

    Add CSS input styles

--- src/components/atoms/InputBox/InputBox.jsx
+++ src/components/atoms/InputBox/InputBox.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.scss'
 
 const InputBox = ({
   id,
   placeHolderText,
-}) => (
-<div className="input-field">
-  <input 
-    id={id} 
-    placeHolder={placeHolderText} 
-    type="text" 
-  />
-  <span>Error message</span>
-</div>);
+  errorText,
+}) => {
+  const [globalState, setGlobalState] = useState('onBlur');
+  useEffect(()=>{
+    if(!errorText) return
+
+    setGlobalState('onError')
+  }, [errorText])
+  return (
+
+    <div className={'input-field ' + globalState}>
+      <input
+        onFocus={() => setGlobalState('onFocus')}
+        onBlur={() => setGlobalState('onBlur')} 
+        className="input-field__input"
+        id={id} 
+        placeHolder={placeHolderText} 
+        type="text" 
+      />
+      {(errorText) && (
+        <span className="input-field__error">
+          Error message
+        </span>
+      )}
+    </div>
+  )
+};
 
 export default InputBox;
