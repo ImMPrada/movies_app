be9839928b3f3c847f7a819b59362a08c7d2469d
miguel <26731448+ImMPrada@users.noreply.github.com>
2022-07-13 22:09:23 -0500

    [WIP] Add login signup forms (#13)
    * Add Register form (#10)
    
    * Add folders structure
    
    * Add Inputfield
    
    * wip
    
    * Add CSS input styles
    
    * Add atoms: InputBox
    
    * add hooks folder
    
    * Add atoms: Button
    
    * Add styles
    
    * Add Movie.svg
    
    * Add styles to RegisterForms
    
    * Add organisms: UserLogForm
    
    * Delete Movie.svg
    
    * Add icons: Movie
    
    * Add organisms: UserLogForm
    
    * Upgrade InputBox
    
    * try
    
    * Add Templates: UserLogForm
    
    * Add page: UserRegisterForm
    
    * Login form (#14)
    
    * Add folders structure
    
    * Add Inputfield
    
    * wip
    
    * Add CSS input styles
    
    * Add atoms: InputBox
    
    * add hooks folder
    
    * Add atoms: Button
    
    * Add styles
    
    * Add Movie.svg
    
    * Add styles to RegisterForms
    
    * Add organisms: UserLogForm
    
    * Delete Movie.svg
    
    * Add icons: Movie
    
    * Add organisms: UserLogForm
    
    * Upgrade InputBox
    
    * try
    
    * Add Templates: UserLogForm
    
    * Add page: UserRegisterForm
    
    * Add login form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {UserLogForm} from './components/organisms'
-import { UserRegisterForm } from './components/pages';
+import {
+  UserLoginForm,
+  UserRegisterForm,
+} from './components/pages';
 import './assets/styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <UserLoginForm />
     <UserRegisterForm />
   </React.StrictMode>
 );
