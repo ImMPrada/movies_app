56d99c9a60cfd4c418ba22a231632d60b7108a3b
immprada <im.mprada@gmail.com>
2022-07-14 22:14:11 -0500

    Add validations file

---
+++ src/components/molecules/LoginForm/validations.js
@@ -0,0 +1,31 @@
+const emptyCheck = {
+  f: function(value){ 
+    console.log(value)
+    return (value === '' || value === null)
+  },
+  prompt: 'Please enter a value'
+}
+
+const validateEmail = {
+  emailReGex: /^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+  f: function(value){ return value.match(this.emailReGex) ? false : true},
+  prompt: 'Please enter a valid email'
+}
+
+
+export const validationSchema = {
+  userEmail: {
+    validations: [
+      {
+        ...emptyCheck,
+        prompt: 'Please enter an email',
+      },
+      validateEmail,
+    ]
+  },
+  password: {
+    validations: [
+      emptyCheck,
+    ]
+  }
+};
