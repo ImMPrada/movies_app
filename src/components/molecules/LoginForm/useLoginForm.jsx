a3f4a4845e111df4c8d73949e4c8d16c93a3079f
immprada <im.mprada@gmail.com>
2022-07-14 22:14:23 -0500

    Add hook for login form

---
+++ src/components/molecules/LoginForm/useLoginForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { validationSchema } from './validations.js'
+
+
+const useLoginForm = (initialValues) => {
+  const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState(() => {
+    const tempErrors = {}
+    Object.keys(values).forEach(key => tempErrors[key] = null)
+    return tempErrors
+  })
+
+
+  // Private
+
+  const runValidations = (validations, value) => {
+    
+    return validations.map(validation => {
+      const isError = validation.f(value)
+      return isError ? validation.prompt : isError
+    })
+
+  }
+
+  const getErrorMessage = (validationResults) => validationResults.filter(result => result != false)[0] || null
+
+
+  // Public
+
+  const updateField = (field, value) => {
+    const currentErrors = errors
+    
+    const validationResults = runValidations(validationSchema[field].validations, value)
+    currentErrors[field] = getErrorMessage(validationResults)
+    setErrors({...currentErrors})
+  }
+
+
+  return {
+    values,
+    errors,
+    updateField,
+  }
+}
+
+export default useLoginForm;
