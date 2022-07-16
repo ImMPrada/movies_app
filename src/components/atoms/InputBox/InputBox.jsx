20c4916bcf5048ded659841fd66c858ec5d20550
immprada <im.mprada@gmail.com>
2022-07-16 09:30:50 -0500

    Fix error style state

--- src/components/atoms/InputBox/InputBox.jsx
+++ src/components/atoms/InputBox/InputBox.jsx
@@ -19,6 +19,12 @@ const InputBox = ({
     if(errorText == '') setGlobalState('onBlur')
   }, [errorText])
 
+  useEffect(() => {
+    if(!globalState) return
+
+    if(globalState == 'onBlur' && errorText != null) setGlobalState('onError')
+  }, [globalState])
+
   return (
 
     <div 
