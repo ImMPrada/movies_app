74d2615bfce74f7c82c776b061cb639ee56a46e1
immprada <im.mprada@gmail.com>
2022-07-17 04:06:57 -0500

    Add example desription

--- src/index.js
+++ src/index.js
@@ -2,21 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Thumbnail} from './components/atoms'
 import './assets/styles/reset.scss';
+import './assets/styles/temporary.scss'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <br/>
+    <br/>
+    <h2> Thumbnail not bookmarked:</h2>
+    <br/>
     <Thumbnail 
       imgSource='https://html.com/wp-content/uploads/flamingo.webp'
       markable
     />
+    <br/>
+    <br/>
+    <h2> Thumbnail bookmarked:</h2>
     <br/>
     <Thumbnail 
       imgSource='https://html.com/wp-content/uploads/flamingo.webp'
       markable
       isMarked
     />
+    <br/>
+    <br/>
+    <h2> Thumbnail of a playable:</h2>
     <br/>
     <Thumbnail 
       imgSource='https://html.com/wp-content/uploads/flamingo.webp'
