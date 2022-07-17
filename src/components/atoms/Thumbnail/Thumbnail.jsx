132f9f58b6a3ebd9b28b519f50f1eb814f701c07
immprada <im.mprada@gmail.com>
2022-07-17 00:29:39 -0500

    Add play button

--- src/components/atoms/Thumbnail/Thumbnail.jsx
+++ src/components/atoms/Thumbnail/Thumbnail.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {PlayVideo} from '../../../assets/icons';
 import './styles.scss'
 
 const Thumbnail = ({
@@ -8,14 +9,19 @@ const Thumbnail = ({
 
 
   return (
+    <>
+    <PlayVideo />
     <div className='thumbnail'>
       <div className="thumbnail__overlay--active">
-        HOLA
+        <button>
+          <PlayVideo />
+        </button>
       </div>
       <div className="thumbnail__base">
         <img  src={imgSource} alt='' />
       </div>
     </div>
+    </>
 
 
   )
