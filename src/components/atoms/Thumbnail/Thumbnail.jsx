5a734eedb8057700074b36673c17ea1557e59e84
immprada <im.mprada@gmail.com>
2022-07-16 22:36:28 -0500

    Init Thumbnail atom

---
+++ src/components/atoms/Thumbnail/Thumbnail.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import './styles.scss'
+
+const Thumbnail = ({
+  id,
+  placeHolderText,
+  errorText,
+}) => {
+
+
+  return (
+
+    <div className={'input-field ' + globalState}>
+
+    </div>
+  )
+};
+
+export default Thumbnail;
